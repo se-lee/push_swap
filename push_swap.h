b70c4e1f7e34c05557556a8eadf51147cda4eb45
selee <selee@student.42lyon.fr>
2021-07-19 00:58:58 +0200

    header and memo

---
+++ push_swap.h
@@ -0,0 +1,48 @@
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include "libft/libft.h"
+
+/*
+typedef struct s_list
+{
+	void			*content;
+	struct s_list	*next;
+}				t_list;
+*/
+
+/*
+stacks
+
+
+*/
+
+
+/*check*/
+//whether arguments are valid: strings, decimals, no argument, ...what else?
+//whether stack arguments are already sorted -> do nothing
+
+/*prep*/
+//prepare stacks a and b
+//create linked list: store each argument to each node and put them into stack a
+
+
+/*operations*/
+//swap sa sb ss
+//push pb pa
+//rotate ra rb rr
+//reverse rotate rra rrb rrr
+
+
+/*display*/
+//display error message
+//display operation name
+//display numbers
+//
+
+
+
+#endif
